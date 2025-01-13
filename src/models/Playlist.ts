83f0e342881f8df30339c4969fdec5c26c1223e3
EliLePissenlit <e.clement1002@gmail.com>
2025-01-13 13:46:23 +0100

    commentaires

--- src/models/Playlist.ts
+++ src/models/Playlist.ts
@@ -3,15 +3,15 @@ import mongoose, { Schema, Document } from 'mongoose';
 // typer le modèle 
 export interface IPlaylist extends Document {
   name: string;
-  description?: string; // Description optionnelle
-  userId: string; // ID de l'utilisateur qui a créé la playlist
-  songs: string[]; // Liste des IDs des chansons dans la playlist
+  description?: string; // je met ? quand je veux que ca soit optionelle 
+  userId: string; 
+  songs: string[]; 
 }
 
 // Schéma 
 const PlaylistSchema: Schema = new Schema({
   name: { type: String, required: true },
-  description: { type: String }, // Optionnel
+  description: { type: String }, 
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Référence à l'utilisateur
   songs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Song' }], // Références aux chansons
 });
