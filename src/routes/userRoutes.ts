4fa4364649431b40035d86a59660c6c8fef9529d
EliLePissenlit <e.clement1002@gmail.com>
2025-01-14 11:45:31 +0100

    login & delete user

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import { Request, Response, Router } from "express";
 import User from '../models/User';
+import bcrypt from "bcryptjs";
 
 const router = Router();
 
+// Ajouter
 router.post('/register', async (req: Request, res: Response) => {
   try {
     const { username, email, password } = req.body;
@@ -19,5 +21,49 @@ router.post('/register', async (req: Request, res: Response) => {
     res.status(500).json({ error: 'Failed to register user' });
   }
 });
+
+// Login
+router.post('/login', async (req: Request, res: Response) => {
+    try { 
+        const { email, password } = req.body;
+
+        // vérification existance user
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: 'Invalid email or passwword}'});
+
+        // vérification mdp avec bcrypt
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) return res.status(400).json({ message: 'Invalid email or password' });
+
+        // génère token JWT
+        const token = user.generateAuthToken();
+
+        res.status(200).json({ user: { username: user.username, email: user.email }, token });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to login user' });
+  }
+
+    })
+
+    // supprimer 
+    router.delete('/:id', async (req: Request, res: Response) => {
+        try {
+          const { id } = req.params;
+      
+          // vérification existance user
+          const user = await User.findById(id);
+          if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+          }
+      
+          // Supprime
+          await User.findByIdAndDelete(id);
+      
+          res.status(200).json({ message: 'User deleted successfully' });
+        } catch (err) {
+          console.error('Erreur lors de la suppression de l\'utilisateur :', err);
+          res.status(500).json({ error: 'Failed to delete user' });
+        }
+      });
 
 export default router;
